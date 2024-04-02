980ce8cb0a34674852376d3012387d461729d976
Bahkmat <161296043+Bahkmat@users.noreply.github.com>
2024-04-02 11:22:19 +0500

    Add files via upload

---
+++ LabaLast-main/WpfApp13/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp13
+{
+    internal class User
+    {
+        public static List<User> Users = new List<User>();
+        public string Login { get; set; }
+        public int Password { get; set; }
+     
+        public User(string _Login, int _Password)
+        {
+            Login = _Login;
+            Password = _Password;
+            Users.Add(this);
+        }
+    }
+}
---
+++ LabaLast-main/WpfApp13/_Pages/Page1.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp13._Pages
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+           foreach (var item in User.Users)
+           {
+                if (item.Login == TBLogin.Text & item.Password == int.Parse(TBPassword.Text)) ;
+                {
+                    MessageBox.Show("вы пошли в систему");
+                }
+           }
+        }
+    }
+}
